05637ec3b34a1121feaa1ea3b973c3d800fd50fe
LOluoch <lavineoluoch@gmail.com>
2024-04-28 09:08:18 -0500

    Static Flask app

---
+++ calc.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template,
+app = Flask(__name__)
+
+@app.route("/LoanCalculator")
+def loan_calc():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
