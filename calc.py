a91710068fa863de08253395f5009ba8990906f2
Lavine Oluoch <lavineoluoch@gmail.com>
2024-04-28 15:35:08 -0500

    url_for

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
